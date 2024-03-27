4ebd8d72f51e8e2fde435d5f5d1a873dc3563ed8
Luis Alcaras <luis.alcaras@improving.com>
2024-03-26 22:08:12 -0600

    feat: :sparkles: SRT file generetion has been implemented

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
-from moviepy.editor import VideoFileClip
 import os
+import json
+from datetime import timedelta
 import speech_recognition as sr
 from pydub import AudioSegment
 
 r = sr.Recognizer()
 
-input_folder = 'assets'
-output_folder = 'out'
+VIDEOS_DIR = 'assets'
+OUTPUT_DIR = 'out'
 # Define the input video file and output audio file
-mp4_files = [f for f in os.listdir(input_folder) if f.endswith('.mp4')]
+videos = [f for f in os.listdir(VIDEOS_DIR) if f.endswith('.mp4')]
 
-for mp4_file in mp4_files:
-    audio = AudioSegment.from_file(os.path.join(input_folder, mp4_file), format="mp4")
-    audio.export(os.path.join(output_folder, mp4_file.replace('.mp4', '.wav')), format="wav")
+for video_name in videos:
+    audio_name = video_name.replace('.mp4', '.wav')
+    text_name = video_name.replace('.mp4', '.txt')
+    json_name = video_name.replace('.mp4', '.json')
 
-audio_files = [f for f in os.listdir(output_folder) if f.endswith('.wav')]
+    video_segment = AudioSegment.from_file(os.path.join(VIDEOS_DIR, video_name), format="mp4")
+    video_segment.export(os.path.join(OUTPUT_DIR, audio_name), format="wav")
 
-for file in audio_files:
-    with sr.AudioFile(os.path.join(output_folder, file)) as source:
+    print(f"Audio extraction of {video_name} is done.")
+
+    with sr.AudioFile(os.path.join(OUTPUT_DIR, audio_name)) as source:
         audio = r.record(source)
         text = r.recognize_google(audio)
-        text_file = open(os.path.join(output_folder, file.replace('.wav', '.txt')), 'w', encoding='utf-8')
+        text_file = open(os.path.join(OUTPUT_DIR, text_name), 'w', encoding='utf-8')
         text_file.write(text)
         text_file.close()
+
+    print(f"Transcription of {video_name} is done.")
+
+    audio_segment = AudioSegment.from_file(os.path.join(OUTPUT_DIR, audio_name), format="wav")
+    word_timestamp = []
+    chunks = len(audio_segment) // 10000
+    for i in range(chunks):
+        start_time = i * 10000
+        end_time = (i + 1) * 10000
+        audio_chunk = audio_segment[start_time:end_time]
+        audio_chunk_name = audio_name.replace('.wav', f"_{i}.wav")
+        audio_chunk.export(os.path.join(OUTPUT_DIR, audio_chunk_name), format="wav")
+
+        with sr.AudioFile(os.path.join(OUTPUT_DIR, audio_chunk_name)) as source:
+            audio = r.record(source)
+            text = r.recognize_google(audio)
+            word_timestamp.append({
+                'start_time': start_time,
+                'end_time': end_time,
+                'text': text
+            })
+
+        os.remove(os.path.join(OUTPUT_DIR, audio_chunk_name))
+    text_file = open(os.path.join(OUTPUT_DIR, json_name), 'w', encoding='utf-8')
+    text_file.write(json.dumps({
+        'word_timestamp': word_timestamp
+    }))
+
+    print(f"Word timestamp of {video_name} is done.")
+
+    # SRT file generation
+    srt_name = video_name.replace('.mp4', '.srt')
+    srt_file = open(os.path.join(OUTPUT_DIR, srt_name), 'w', encoding='utf-8')
+
+    for i, wt in enumerate(word_timestamp):
+        srt_file.write(f"{i + 1}\n0{timedelta(milliseconds=wt['start_time'])},000 --> {timedelta(milliseconds=wt['end_time'])},000\n")
+        srt_file.write(f"{wt['text']}\n\n")
+
+    srt_file.close()
+    print(f"SRT generation of {video_name} is done.")
